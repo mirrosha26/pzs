fab7d1076ff201a6fd590ea10dcc478f297b7df2
Mikhail Miroshnikov <mirrosha26@gmail.com>
2025-01-25 03:31:08 +0300

    ✨ feat: add task 3 for practice assignment 2

---
+++ MyApplication/app/src/main/java/com/mirrosha/myapplication/MainActivity.java
@@ -0,0 +1,33 @@
+package com.mirrosha.myapplication;
+
+import androidx.appcompat.app.ActionBar;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+import android.os.Bundle;
+import android.view.MenuItem;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_scrolling);
+        Toolbar toolbar = findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        ActionBar actionBar = getSupportActionBar();
+        if (actionBar != null) {
+            actionBar.setDisplayHomeAsUpEnabled(true);
+            actionBar.setTitle("Путешествия Гулливера");
+        }
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        if (item.getItemId() == android.R.id.home) {
+            onBackPressed();
+            return true;
+        }
+        return super.onOptionsItemSelected(item);
+    }
+}
--- README.md
+++ README.md
@@ -3,7 +3,7 @@
 ## ПЗ 1
 Разработка приложения "Здравствуй, мир!"
 
-<img src="media/pz1app/sreen1.png" width="300" /> <img src="media/pz1app/sreen2.png" width="300" /> 
+<img src="media/pz_1app/sreen1.png" width="300" /> <img src="media/pz_1app/sreen2.png" width="300" /> 
 
 Исходный код приложения: [/pz1app](/pz1app)
 
@@ -15,8 +15,12 @@
 <img src="media/pz_2app/screen1-1.png" width="300" /> <img src="media/pz_2app/screen1-2.png" width="300" /> 
 
 Исходный код приложения: [/pz_2](/pz_2)
-
 ### Задание 2
 <img src="media/pz_2app/screen2-1.png" width="600" />
 
 Исходный код приложения: [/pz22app](/pz22app)
+
+### Задание 3
+<img src="media/pz_2app/screen3-1.png" width="600" />
+
+Исходный код приложения: [/pz23app](/pz23app)
